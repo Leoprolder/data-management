89b1b1ff35e2399b47fcd260c155058ab13b8620
leopold.1996@inbox.ru <leopold.1996@inbox.ru>
2025-03-06 13:35:43 +0300

    Update angular version

--- data-management-app/src/app/app.component.spec.ts
+++ data-management-app/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'data-management-app'`, () => {
+  it(`should have the 'data-management-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('data-management-app');
@@ -30,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('data-management-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, data-management-app');
   });
 });
